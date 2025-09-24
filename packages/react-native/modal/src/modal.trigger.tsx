afade9c2bd11f57752043fda2f1daa5e793d30d6
Nicolás CABELLO <nicolas.cabello@irrigacion.gov.ar>
2025-09-24 09:56:54 -0300

    Ahora el modal de react-native permite usar elementos presionables en el trigger

--- packages/react-native/modal/src/modal.trigger.tsx
+++ packages/react-native/modal/src/modal.trigger.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import { Pressable } from 'react-native';
+import { Children, cloneElement, isValidElement } from 'react';
+import { GestureResponderEvent } from 'react-native';
 import { useModal } from './modal.context';
 import { TriggerProps } from './modal.types';
 
+interface TriggerChildProps {
+	onPress?: (event: GestureResponderEvent) => Promise<void> | void;
+}
+
 export const Trigger = ({ children }: TriggerProps) => {
 	const { setOpen } = useModal();
-	return (
-		<Pressable onPress={() => setOpen(true)}>
-			{typeof children === 'function' ? children({ pressed: false }) : children}
-		</Pressable>
-	);
+
+	const child = Children.only(children);
+
+	if (!isValidElement<TriggerChildProps>(child)) return null;
+
+	return cloneElement(child, {
+		...child.props,
+		onPress: async (event: GestureResponderEvent) => {
+			await child.props.onPress?.(event);
+			setOpen(true);
+		},
+	});
 };
