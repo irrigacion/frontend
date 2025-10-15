0b7e5bb1553ac86062c718d83de00b58b701fca2
Nicolas Cabello <cabellonic@gmail.com>
2025-10-15 14:57:02 -0300

    Ahora se puede utilizar una referencia en el componente Content del Modal

--- packages/react-native/modal/src/modal.content.tsx
+++ packages/react-native/modal/src/modal.content.tsx
@@ -1,4 +1,4 @@
-import { Children, isValidElement, useEffect, useRef, useState } from 'react';
+import { Children, forwardRef, isValidElement, useEffect, useRef, useState } from 'react';
 import {
 	Animated,
 	Modal as BaseModal,
@@ -16,124 +16,126 @@ import { Header } from './modal.header';
 import { styles } from './modal.style';
 import { ContentProps, ModalSize } from './modal.types';
 
-export const Content = ({ children, onMount, onUnmount, style }: ContentProps) => {
-	const { open, setOpen, config } = useModal();
-	const window = Dimensions.get('window');
+export const Content = forwardRef<ScrollView, ContentProps>(
+	({ children, onMount, onUnmount, style }, ref) => {
+		const { open, setOpen, config } = useModal();
+		const window = Dimensions.get('window');
 
-	const [isVisible, setIsVisible] = useState(open);
+		const [isVisible, setIsVisible] = useState(open);
 
-	const opacity = useRef(new Animated.Value(0)).current;
-	const scale = useRef(new Animated.Value(0.8)).current;
+		const opacity = useRef(new Animated.Value(0)).current;
+		const scale = useRef(new Animated.Value(0.8)).current;
 
-	useEffect(() => {
-		if (!isVisible) return;
+		useEffect(() => {
+			if (!isVisible) return;
 
-		return () => {
-			onUnmount?.();
+			return () => {
+				onUnmount?.();
+			};
+		}, [isVisible, onUnmount]);
+
+		useEffect(() => {
+			if (open) {
+				setIsVisible(true);
+				Animated.parallel([
+					Animated.timing(opacity, { toValue: 1, duration: 0, useNativeDriver: true }),
+					Animated.spring(scale, {
+						toValue: 1,
+						bounciness: 5,
+						speed: 50,
+						useNativeDriver: true,
+					}),
+				]).start(() => {
+					onMount?.();
+				});
+			} else {
+				Animated.parallel([
+					Animated.timing(opacity, { toValue: 0, duration: 150, useNativeDriver: true }),
+					Animated.timing(scale, { toValue: 0.8, duration: 150, useNativeDriver: true }),
+				]).start(() => setIsVisible(false));
+			}
+		}, [open]);
+
+		let header: React.ReactElement | null = null;
+		let footer: React.ReactElement | null = null;
+		const bodyContent: React.ReactNode[] = [];
+
+		Children.forEach(children, (child) => {
+			if (isValidElement<ContentProps>(child)) {
+				const childType = child.type as React.ComponentType<any> & { displayName: string };
+				if (childType.displayName === Header.displayName) header = child;
+				else if (childType.displayName === Footer.displayName) footer = child;
+				else bodyContent.push(child);
+			}
+		});
+
+		if (!isVisible) return null;
+
+		const dimensions: Record<ModalSize, StyleProp<ViewStyle>> = {
+			auto: { width: '90%', maxHeight: '90%' },
+			lg: { height: window.height * 0.8, width: '90%' },
+			fullscreen: { height: '100%', width: '100%' },
 		};
-	}, [isVisible, onUnmount]);
 
-	useEffect(() => {
-		if (open) {
-			setIsVisible(true);
-			Animated.parallel([
-				Animated.timing(opacity, { toValue: 1, duration: 0, useNativeDriver: true }),
-				Animated.spring(scale, {
-					toValue: 1,
-					bounciness: 5,
-					speed: 50,
-					useNativeDriver: true,
-				}),
-			]).start(() => {
-				onMount?.();
-			});
-		} else {
-			Animated.parallel([
-				Animated.timing(opacity, { toValue: 0, duration: 150, useNativeDriver: true }),
-				Animated.timing(scale, { toValue: 0.8, duration: 150, useNativeDriver: true }),
-			]).start(() => setIsVisible(false));
-		}
-	}, [open]);
+		const sizeStyle = dimensions[config.size || 'auto'];
 
-	let header: React.ReactElement | null = null;
-	let footer: React.ReactElement | null = null;
-	const bodyContent: React.ReactNode[] = [];
+		return (
+			<BaseModal
+				visible={isVisible}
+				transparent
+				animationType='none'
+				onRequestClose={() => setOpen(false)}
+			>
+				<View style={{ flex: 1 }} pointerEvents='box-none'>
+					{/* Backdrop */}
+					<TouchableWithoutFeedback onPress={() => setOpen(false)}>
+						<Animated.View
+							style={[styles.backdrop, { opacity }]}
+							pointerEvents='box-only'
+						/>
+					</TouchableWithoutFeedback>
 
-	Children.forEach(children, (child) => {
-		if (isValidElement<ContentProps>(child)) {
-			const childType = child.type as React.ComponentType<any> & { displayName: string };
-			if (childType.displayName === Header.displayName) header = child;
-			else if (childType.displayName === Footer.displayName) footer = child;
-			else bodyContent.push(child);
-		}
-	});
-
-	if (!isVisible) return null;
-
-	const dimensions: Record<ModalSize, StyleProp<ViewStyle>> = {
-		auto: { width: '90%', maxHeight: '90%' },
-		lg: { height: window.height * 0.8, width: '90%' },
-		fullscreen: { height: '100%', width: '100%' },
-	};
-
-	const sizeStyle = dimensions[config.size || 'auto'];
-
-	return (
-		<BaseModal
-			visible={isVisible}
-			transparent
-			animationType='none'
-			onRequestClose={() => setOpen(false)}
-		>
-			<View style={{ flex: 1 }} pointerEvents='box-none'>
-				{/* Backdrop */}
-				<TouchableWithoutFeedback onPress={() => setOpen(false)}>
-					<Animated.View
-						style={[styles.backdrop, { opacity }]}
-						pointerEvents='box-only'
-					/>
-				</TouchableWithoutFeedback>
-
-				{/* Contenedor centrado */}
-				<View
-					style={[
-						styles.centeredContainer,
-						{ padding: config.size === 'fullscreen' ? 0 : 16 },
-					]}
-				>
-					<Animated.View
+					{/* Contenedor centrado */}
+					<View
 						style={[
-							styles.contentWrapper,
-							sizeStyle,
-							{
-								borderRadius: config.size === 'fullscreen' ? 0 : 8,
-								maxHeight: config.size === 'fullscreen' ? '100%' : '90%',
-								transform: [{ scale }],
-							},
+							styles.centeredContainer,
+							{ padding: config.size === 'fullscreen' ? 0 : 16 },
 						]}
 					>
-						{/* Header */}
-						{header}
+						<Animated.View
+							style={[
+								styles.contentWrapper,
+								sizeStyle,
+								{
+									borderRadius: config.size === 'fullscreen' ? 0 : 8,
+									maxHeight: config.size === 'fullscreen' ? '100%' : '90%',
+									transform: [{ scale }],
+								},
+							]}
+						>
+							{/* Header */}
+							{header}
 
-						{/* Body */}
-						<TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-							<ScrollView
-								style={config.size === 'auto' ? {} : { flex: 1 }}
-								contentContainerStyle={[styles.body, style]}
-								keyboardShouldPersistTaps='handled'
-								keyboardDismissMode='on-drag'
-							>
-								{bodyContent}
-							</ScrollView>
-						</TouchableWithoutFeedback>
+							{/* Body */}
+							<TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+								<ScrollView
+									style={config.size === 'auto' ? {} : { flex: 1 }}
+									contentContainerStyle={[styles.body, style]}
+									keyboardShouldPersistTaps='handled'
+									keyboardDismissMode='on-drag'
+								>
+									{bodyContent}
+								</ScrollView>
+							</TouchableWithoutFeedback>
 
-						{/* Footer */}
-						{footer}
-					</Animated.View>
+							{/* Footer */}
+							{footer}
+						</Animated.View>
+					</View>
 				</View>
-			</View>
-		</BaseModal>
-	);
-};
+			</BaseModal>
+		);
+	},
+);
 
 Content.displayName = 'ModalContent';
